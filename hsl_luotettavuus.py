c4a8caf392a06fe310791c21f0973946cfe4e449
villeuusirauva-create <ville.uusirauva@gmail.com>
2026-05-23 10:00:14 +0300

    Update hsl_luotettavuus.py

--- hsl_luotettavuus.py
+++ hsl_luotettavuus.py
@@ -31,7 +31,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
  
 # Analysoitava päivä: None = eilen automaattisesti
 # Tietty päivä: "2026-05-20"
-ANALYSOITAVA_PAIVA = None
+ANALYSOITAVA_PAIVA = "2026-05-21"
  
 # Rinnakkaiset lataukset
 RINNAKKAISET_LATAUKSET = 3
