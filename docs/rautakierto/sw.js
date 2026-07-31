b2f2001978fbbc4759ac3504fed25fae11170428
villeuusirauva-create <ville.uusirauva@gmail.com>
2026-07-31 16:57:16 +0300

    Add files via upload

---
+++ docs/rautakierto/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'rautakierto-v3';
+const ASSETS = ['./', './index.html', './manifest.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  // Verkko ensin HTML-sivulle, jotta päivitykset näkyvät heti; muu sisältö välimuistista.
+  if(e.request.mode === 'navigate'){
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match(e.request))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request))
+  );
+});
